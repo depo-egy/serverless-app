ef74bd73619e0670d3e35f9262df6c5ee9a3b18a
depo-egy <mohammad.diab1991@gmail.com>
2020-10-01 17:51:31 +0200

    second commit

--- backend/src/dataLayer/todoAccess.ts
+++ backend/src/dataLayer/todoAccess.ts
@@ -74,7 +74,7 @@ export class TodoAccess {
       },
       UpdateExpression: "set attachmentUrl = :signedUrl",
       ExpressionAttributeValues: {
-        ":signedUrl": "https://ms10596-todo.s3.amazonaws.com/" + todoId
+        ":signedUrl": "https://depo-todo-todo.s3.amazonaws.com/" + todoId
       },
       ReturnValues: "UPDATED_NEW"
     }).promise()
